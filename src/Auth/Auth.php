b7ef9f1ece4cf6744f97e9787c638e433de4b639
Jesse87275 <67485119+JesseSoeteman@users.noreply.github.com>
2023-01-23 16:05:20 +0100

    Abstract class should not be used.
    Set up a constructor for testing.

--- src/Auth/Auth.php
+++ src/Auth/Auth.php
@@ -2,10 +2,24 @@
 
 namespace APIHandler\Auth;
 
-abstract class Auth {
+use APIHandler\APIHandler;
+
+class Auth {
+
+    private APIHandler $apiHandler;
+
+    public function __construct(APIHandler &$apiHandler) {
+
+        if (!isset($apiHandler)) {
+            $this->apiHandler = new APIHandler();
+            $this->apiHandler->addError("APIHandler not initialized", true);
+        }
+
+        $this->apiHandler = $apiHandler;
+    }
 
     public function InitSession() {
-        echo "InitSession";
+        $this->apiHandler->addError("Session not initialized", true);
     }
 
     public function GetSessionVariable() {
