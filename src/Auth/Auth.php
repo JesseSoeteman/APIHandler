64c4bc94ddbc88b068fb6c6ba4ed589c7e3f54fe
Jesse87275 <67485119+JesseSoeteman@users.noreply.github.com>
2023-02-24 15:18:45 +0100

    debugging

--- src/Auth/Auth.php
+++ src/Auth/Auth.php
@@ -67,10 +67,10 @@ class Auth
             $this->apiHandler->addError("Private key could not be generated", true);
         }
 
-        // $this->apiHandler->addData([
-        //     "server_privateKey" => $server_privateKey,
-        //     "client_publicKey" => $client_publicKey
-        // ], true);
+        $this->apiHandler->addData([
+            "server_privateKey" => $server_privateKey,
+            "client_publicKey" => json_encode($client_publicKey)
+        ], true);
 
         // Create a shared secret, and check if it is valid
         $shared_secret = openssl_dh_compute_key(hex2bin($client_publicKey), $server_privateKey);
